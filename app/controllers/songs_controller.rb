97d6a2d2e9fe7294ccdc3b3bd23ce82498745997
Micah Banschick <micha.ban@gmail.com>
2020-12-15 18:51:31 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,9 @@
 class SongsController < ApplicationController
   def index
     @songs = Song.all
+    if @songs.any{|song| song.artist == nil}
+      redirect_to artists_path
+    end 
   end
 
   def show
